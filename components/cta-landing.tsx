87bd5a683652d588868460d4d9758f2b1eec8e8a
Farhaan Patel <farhaanp99@gmail.com>
2023-05-29 21:04:02 -0400

    added cta landing page message. replace lorem text

--- components/cta-landing.tsx
+++ components/cta-landing.tsx
@@ -7,35 +7,29 @@ export default function CTA() {
       <div className="row-span-full p-8 md:p-12 lg:px-16 lg:py-24">
         <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
           <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            Command the Future, Secure Your Destiny
           </h2>
 
           <p className="hidden text-gray-500 md:mt-4 md:block">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Et, egestas tempus tellus etiam sed. Quam a scelerisque
-            amet ullamcorper eu enim et fermentum, augue. Aliquet amet
-            volutpat quisque ut interdum tincidunt duis.
+            Embrace the opportunity to shape our society as a Commander in Gilead. The flag of Gilead symbolizes our shared authority, leadership, and purpose. Join us to secure your place in our legacy.
           </p>
 
-
-          <div
-                className="mx-auto max-w-xs sm:flex flex-col md:flex-row sm:max-w-none sm:justify-center mt-6">
-
-          <div>
-            <Link
-              className="btn mb-4 w-full bg-red-600 text-white hover:bg-red-700 sm:mb-0 sm:w-auto"
-              href="/contact">
-              Join the Ranks
-            </Link>
+          <div className="mx-auto mt-6 max-w-xs flex-col sm:flex sm:max-w-none sm:justify-center md:flex-row">
+            <div>
+              <Link
+                className="btn mb-4 w-full bg-red-600 text-white hover:bg-red-700 sm:mb-0 sm:w-auto"
+                href="/contact">
+                Join the Ranks
+              </Link>
+            </div>
+            <div>
+              <Link
+                className="btn w-full bg-gray-900 text-white hover:bg-gray-800 sm:ml-4 sm:w-auto"
+                href="/blog">
+                Learn more
+              </Link>
+            </div>
           </div>
-          <div>
-            <Link
-              className="btn w-full bg-gray-900 text-white hover:bg-gray-800 sm:ml-4 sm:w-auto"
-              href="/blog">
-              Learn more
-            </Link>
-          </div>
-                </div>
         </div>
       </div>
 
